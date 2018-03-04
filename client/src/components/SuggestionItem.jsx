111f6348251908e4be86a44c26c7f304dc1bf875
girikalra <girikalra@gmail.com>
2018-03-04 12:39:00 -0800

    Add styling to Slider

--- client/src/components/SuggestionItem.jsx
+++ client/src/components/SuggestionItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 
 class SuggestionItem extends React.Component {
   constructor(props) {
@@ -6,14 +7,16 @@ class SuggestionItem extends React.Component {
   }
 
   render() {
-    
+    var time = moment.parseZone(this.props.event.time).local().format('MMMM Do YYYY @ h:mm a')
+    var title = this.props.event.title.length > 55 ? this.props.event.title.substring(0,55) + '...' : this.props.event.title;
+    var category = this.props.event.category.toLowerCase();
     return (
-      <div>
-        <img src={this.props.event.photo} height={90} width={160}></img>
-        <div>{this.props.event.title}</div>
-        <div>{this.props.event.time}</div>
-        <div>{this.props.event.location}</div>
-        <div>{this.props.event.category}</div>
+      <div className="event-suggestion-container">
+        <div className="event-photo"><img src={this.props.event.photo} height={140.63} width={250}></img></div>
+        <div className="event-title">{title}</div>
+        <div className="event-time">{time}</div>
+        <div className="event-location">{this.props.event.location}</div>
+        <div className="event-category">#{category}</div>
       </div>
     )
   }
--- client/src/components/SuggestionsList.jsx
+++ client/src/components/SuggestionsList.jsx
@@ -14,38 +14,15 @@ class SuggestionsList extends React.Component {
     }
 
     const settings =  {
-      dots: false,
+      dots: true,
       infinite: false,
       speed: 300,
-      slidesToShow: 4,
+      slidesToShow: 2,
       slidesToScroll: 1,
-      responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 770,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]};
+    };
 
     return (
       <div>
-        <div><b>Similar Events</b></div>
         {this.props.events.length > 0 && <Slider {...settings}>
           {this.props.events.map((item, index) => {
             return  <div key={index}> <SuggestionItem event={item} /> </div>
